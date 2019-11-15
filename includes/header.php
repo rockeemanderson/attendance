c7c70821a3c7885b7bf21babe24bb669e3c1e702
rockeemanderson <56086450+rockeemanderson@users.noreply.github.com>
2019-11-15 16:03:39 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -28,7 +28,7 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="viewrecords.php">View Attendees</a>
-          </li>
+         </li><!-- 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Dropdown
@@ -42,7 +42,7 @@
           </li>
           <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
+          </li> -->
         </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
